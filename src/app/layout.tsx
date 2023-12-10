297bcc3d86167c6ad79de2a5b633a0d0a84820e7
eisec <info@eisec.ru>
2023-12-10 16:33:47 +0300

    08.12.23

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next'
 import './globals.css'
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Cookie from "@/components/cookie";
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -18,6 +19,7 @@ export default function RootLayout({
         <body>
         <Header/>
         {children}
+        <Cookie/>
         <Footer/>
         </body>
         </html>
