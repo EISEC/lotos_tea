6c469f7137f838da0e1c20edac89469f5fd1c7f4
eisec <info@eisec.ru>
2023-12-20 15:10:38 +0300

    20.12.23

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,6 @@
 'use server'
 import React from 'react';
+import Image from "next/image";
 
 async function getData() {
     const res = await fetch('https://ifuw.ru/lotos/wp-json/api/page/home', {next: {revalidate: 0}})
@@ -21,14 +22,18 @@ const Hero = async () => {
         <section>
             <div
                 className="relative container mx-auto w-full px-4 py-32 sm:px-6 lg:flex lg:h-[50vh] lg:items-center lg:px-8 gap-10 justify-between">
-                <div className="max-w-xl text-left">
-                    <h1 className="text-3xl font-extrabold sm:text-5xl" dangerouslySetInnerHTML={{__html: data[0]['block1'][0]['zagolovok_na_sekczii']}}/>
+                <div className="w-full text-left">
+                    <h1 className="text-3xl font-extrabold sm:text-5xl"
+                        dangerouslySetInnerHTML={{__html: data[0]['block1'][0]['zagolovok_na_sekczii']}}/>
 
                     <p className="mt-4 max-w-lg sm:text-xl/relaxed">
                         {data[0]['block1'][0]['opisanie_sekczii']}
                     </p>
                 </div>
-                <img src={img} alt=""/>
+                <div className={'relative h-full w-full flex items-end justify-end'}>
+                    <Image src={img} fill={true}
+                           style={{objectFit: "contain"}} alt=""/>
+                </div>
             </div>
         </section>
     );
--- src/components/singlePost.tsx
+++ src/components/singlePost.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 const SinglePost = ({data}: any) => {
     const title = data[0].title.rendered
     const content = data[0].content.rendered
-    const imge = data[0]['_embedded']["wp:featuredmedia"][0].source_url
+    let imge = data[0]['_embedded']["wp:featuredmedia"][0].source_url === undefined ? false : data[0]['_embedded']["wp:featuredmedia"][0].source_url
     return (
         <main>
             <header className={'container mx-auto relative h-64'}>
                 <div className={'bg-black/50 w-full h-full px-6'}>
-                    <Image className={'-z-10 absolute'} src={imge} alt={title} fill={true} loading="lazy"
-                           sizes="(max-width: 768px) 100vw, 33vw" style={{objectFit: "cover"}}/>
+                    {imge ? <Image className={'-z-10 absolute'} src={imge} alt={title} fill={true} loading="lazy"
+                           sizes="(max-width: 768px) 100vw, 33vw" style={{objectFit: "cover"}}/>: ''}
                     <div className="flex items-center justify-center h-full w-full">
                         <div className="text-center">
                             <h1 className="text-white text-2xl font-semibold uppercase md:text-3xl mb-2"
