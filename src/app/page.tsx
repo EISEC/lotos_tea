db808ff0842c599aeb15e9faea7af4361a5ae181
eisec <info@eisec.ru>
2023-12-28 04:02:33 +0300

    20.12.23

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/hero";
 import Catalog from "@/components/catalog";
 import HomeContent from "@/components/home_content";
 import BlogPost from "@/components/blogPost";
-import BrandFilter from "@/components/brandFilter";
 import HomeBrend from "@/components/homeBrend";
 
 export default function Home() {
