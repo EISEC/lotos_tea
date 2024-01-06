e0349b2cd4060176ed530222e7a8ce7250006976
eisec <info@eisec.ru>
2024-01-06 03:48:51 +0300

    06.01.24

--- src/components/homeProductSlider.tsx
+++ src/components/homeProductSlider.tsx
@@ -3,16 +3,17 @@ import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
 import {Swiper, SwiperSlide} from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/pagination';
 import {Navigation} from 'swiper/modules';
+
+// import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const HomeProductCards = ({dta}: any) => {
     return (
         <Swiper
             slidesPerView={1}
-            autoplay={true}
-            navigation={true}
+            navigation
             breakpoints={{
                 640: {
                     slidesPerView: 2,
@@ -25,15 +26,15 @@ const HomeProductCards = ({dta}: any) => {
                 },
             }}
             modules={[Navigation]}
-            className="mySwiper"
+            className="mySwiper md:px-6"
         >
 
             {
                 dta.slice(0, 8).map((el: any) => {
                     return (
-                        <SwiperSlide key={el.id} className={'my-8 p-4'}>
+                        <SwiperSlide key={el.id} className={'mt-8 px-4'}>
                             <div
-                                className="h-full w-full flex flex-col justify-center relative overflow-hidden border-2 border-emerald-600 rounded-lg max-w-xs shadow-md">
+                                className="h-full w-full flex flex-col justify-center relative overflow-hidden border-2 border-emerald-600 rounded-lg shadow-md">
                                 <div className="relative pt-3 flex items-center justify-center">
                                     <Link className={'relative w-40 h-60'} href={`/catalog/${el.slug}`}>
                                         <Image
@@ -45,7 +46,8 @@ const HomeProductCards = ({dta}: any) => {
                                     </Link>
                                 </div>
                                 <div className="relative px-6 pb-6 mt-6 flex flex-col gap-3">
-                                    <span className="block opacity-75 -mb-1">{el.cat[0].name}</span>
+                                    <span
+                                        className="block opacity-75 -mb-1">{!el.cat ? 'Нет производителя' : el.cat[0]['name']}</span>
                                     <Link href={`/catalog/${el.slug}`} className="flex justify-between flex-col gap-2">
                                         <span className="block font-semibold text-xl">{el.title}</span>
                                     </Link>
